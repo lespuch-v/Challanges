53e392f5ed7f6e09f0c0f5d9a4646cf2a82472f2
lespuch-v <v.lespukh@gmail.com>
2021-10-05 23:32:44 +0200

    foodish upload

---
+++ Front End Challenges/HTML - CSS - JS - Foodish API/scripts.js
@@ -0,0 +1,10 @@
+const myButton = document.querySelector(".button-24")
+const image = document.querySelector(".justAnImage")
+
+
+myButton.addEventListener("click", async()=>{
+    const res = await fetch("https://foodish-api.herokuapp.com/api/")
+    const data = await res.json()
+    image.src = `${data.image}`
+    console.log(data.image)
+})
