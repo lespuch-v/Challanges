1fb610ca5f456f5f5baeb1252b33e931b3537baa
lespuch-v <v.lespukh@gmail.com>
2021-10-05 23:34:37 +0200

    fix

--- Front End Challenges/HTML - CSS - JS - Foodish API/scripts.js
+++ Front End Challenges/HTML - CSS - JS - Foodish API/scripts.js
@@ -2,9 +2,11 @@ const myButton = document.querySelector(".button-24")
 const image = document.querySelector(".justAnImage")
 
 
-myButton.addEventListener("click", async()=>{
+myButton.addEventListener("click", myFunk())
+
+async function myFunk(){
     const res = await fetch("https://foodish-api.herokuapp.com/api/")
     const data = await res.json()
     image.src = `${data.image}`
     console.log(data.image)
-})
+}
